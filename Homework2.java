506427f632d000b271dd1ba15e39c438e51720a6
shoshinmas <shoshinmas@protonmail.com>
2019-10-14 21:19:38 +0200

    Home work from 10.10.19

---
+++ Homework2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Homework2 {
+
+    private static int discount;
+    public static int Coupon(int a) {
+        if (a > 65)
+        {
+            discount = 10;
+        }
+        else if (a < 18) {
+            discount = 25;
+        }
+        else {
+            discount = 0;
+        }
+
+        return discount;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("What is your age:");
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+        Coupon(age);
+        boolean regular = true;
+        System.out.println("Is the customer new or regular?");
+        String ifRegular = scanner.next();
+            if (ifRegular == "regular") {
+                regular = true;
+            }
+            else {
+                regular = false;
+            }
+        if (regular = true) {
+            discount =+5;
+            System.out.println("Rabat dla klienta to " + discount+"%");
+        }
+        else {
+            System.out.println("Rabat dla klienta to " + discount+"%");
+        }
+
+    }
+}
